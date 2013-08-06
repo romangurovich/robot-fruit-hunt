669a6cf55ecd1e4ee5351b1fd73212f8c475aef6
Roman Gurovich <romangurovich@gmail.com>
2013-08-05 18:00:40 -0700

    Clone SimpleBot

--- mybot.js
+++ mybot.js
@@ -5,24 +5,89 @@ function make_move() {
    var board = get_board();
 
    // we found an item! take it!
-   if (board[get_my_x()][get_my_y()] > 0) {
-       return TAKE;
+   if (has_item(board[get_my_x()][get_my_y()])) {
+      return TAKE;
    }
 
-   var rand = Math.random() * 4;
+   // looks like we'll have to keep track of what moves we've looked at
+   toConsider = new Array();
+   considered = new Array(HEIGHT);
+   for (var i = 0; i < WIDTH; i++) {
+      considered[i] = new Array(HEIGHT);
+      for (var j = 0; j < HEIGHT; j++) {
+         considered[i][j] = 0;
+      }
+   }
 
-   if (rand < 1) return NORTH;
-   if (rand < 2) return SOUTH;
-   if (rand < 3) return EAST;
-   if (rand < 4) return WEST;
-
-   return PASS;
+   // let's find the move that will start leading us to the closest item
+   return findMove(new node(get_my_x(), get_my_y(), -1));
 }
 
-// Optionally include this function if you'd like to always reset to a 
-// certain board number/layout. This is useful for repeatedly testing your
-// bot(s) against known positions.
-//
-//function default_board_number() {
-//    return 123;
-//}
+function findMove(n) {
+   // closest item! we will go to it
+   if (has_item(get_board()[n.x][n.y])) {
+      return n.move;
+   }
+
+   var possibleMove = n.move;
+
+   // NORTH
+   if (considerMove(n.x, n.y-1)) {
+      if (n.move == -1) {
+         possibleMove = NORTH;
+      } 
+      toConsider.push(new node(n.x, n.y-1, possibleMove));
+   } 
+
+   // SOUTH
+   if (considerMove(n.x, n.y+1)) {
+      if (n.move == -1) {
+         possibleMove = SOUTH;
+      } 
+      toConsider.push(new node(n.x, n.y+1, possibleMove));
+   } 
+
+   // WEST
+   if (considerMove(n.x-1, n.y)) {
+      if (n.move == -1) {
+         possibleMove = WEST;
+      } 
+      toConsider.push(new node(n.x-1, n.y, possibleMove));
+   } 
+
+   // EAST 
+   if (considerMove(n.x+1, n.y)) {
+      if (n.move == -1) {
+         possibleMove = EAST;
+      }
+      toConsider.push(new node(n.x+1, n.y, possibleMove));
+   } 
+
+   // take next node to bloom out from
+   if (toConsider.length > 0) {
+      var next = toConsider.shift();
+      return findMove(next);
+   }
+
+   // no move found
+   return -1;
+}
+
+function considerMove(x, y) {
+   if (!isValidMove(x, y)) return false;
+   if (considered[x][y] > 0) return false;
+   considered[x][y] = 1;
+   return true;
+}
+
+function isValidMove(x, y) {
+   if (x < 0 || y < 0 || x >= WIDTH || y >= HEIGHT) return false;
+   return true;
+}
+
+function node(x, y, move) {
+   this.x = x;
+   this.y = y;
+   this.move = move;
+}
+
